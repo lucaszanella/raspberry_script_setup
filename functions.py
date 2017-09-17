2857821ecb6891d00855156b692362492ae9c618
Lucas Zanella <lucas@localhost.localdomain>
2017-09-17 01:35:32 -0300

    small mods

--- functions.py
+++ functions.py
@@ -8,6 +8,9 @@ from hashlib import sha256 #Forget MD5, its broken
 import base64
 
 #FILE EDITING TOOLS -------------------------------------
+def log(message):
+    print(message + "...")
+
 def read_file(path):
     f = open(path,'r')
     filedata = f.read()
@@ -16,6 +19,7 @@ def read_file(path):
 
 #https://stackoverflow.com/a/22876912
 def backup_file(path):
+    log("Creating backup of " + path)
     f = open(path,'r')
     filedata = f.read()
     f.close()
@@ -29,21 +33,56 @@ def replace(content, rules):
         content = re.sub(re.compile('^(?!#)' + rule[0] + '$', re.MULTILINE), rule[1], content, 0)
     return content
 
+def is_file_or_symlink_to_file(path):
+    if os.path.islink(path):
+        print("is link: " + path + os.readlink(path))
+        print(path + "/" + os.readlink(path))
+        if os.path.isfile(path + "/" + os.readlink(path)):
+            print("is file on link: " + path)
+            return True
+        else:
+            return False
+    elif os.path.isfile(path):
+        return True
+    else:
+        return False
 def list_files(path):
-    listOfFiles = [f for f in os.listdir(path) if os.path.isfile(f)]
+    listOfFiles = [f for f in os.listdir(path) if is_file_or_symlink_to_file(path.rpartition("/")[0]+"/"+f)]
     return listOfFiles
 
-def enable_rc_service(service_name):
+#RC services are old, but raspibian uses a compatiblity trick: https://unix.stackexchange.com/questions/233468/how-does-systemd-use-etc-init-d-scripts 
+def disable_rc_service(raspbian_root, service_name):
+    log("Disabling " + service_name + " service")  
+    modify_rc_service(raspbian_root, service_name, action="disable")
+  
+def enable_rc_service(raspbian_root, service_name):
+    log("Enabling " + service_name + " service")
+    modify_rc_service(raspbian_root, service_name, action="enable")
+  
+def modify_rc_service(raspbian_root, service_name, action=None):
     for i in range(0,6):
-        for file in list_files(raspbian_root + "etc/rc"+i+".d"):
-            if file == "service_name":
-                pass #Something that searches for the service name
+        rc_folder = raspbian_root + "etc/rc"+str(i)+".d/"
+        print("rc_folder: " + rc_folder)
+        print("files: " + str(list_files(rc_folder)))
+        for file in list_files(rc_folder):
+            print(file)
+            if re.match('[SK][0-9][0-9]' + service_name, file):
+                if action=="enable":
+                    rename_file(rc_folder + file, "S" + file[1:3] + service_name)
+                    log("Service enabled")
+                elif action=="disable":
+                    rename_file(rc_folder + file, "K" + file[1:3] + service_name)
+                    log("Service disabled")                    
+            else:
+                log("Enabling service gone wrong")
 
 def replace_in_file(file, rules):
+    log("Replacing content of  " + file + " with rules " + str(rules))    
     return replace(read_file(file), rules)
 
-def change_file_permissions(file, new_permission):
-    os.chmod(file, new_permission) #Read about python permissions: https://docs.python.org/3/library/stat.html#stat.S_IRWXU
+def modify_file_permissions(file, new_permission):
+    log("Modifying permissions from " + file + " to " + str(new_permission))
+    os.chmod(file, new_permission) #Read about python permissions nomenclature: https://docs.python.org/3/library/stat.html#stat.S_IRWXU
 
 '''
 Usage: 
@@ -52,19 +91,24 @@ or
 rename_file("old_file_name", "new_file_name") will change old_file_name to new_file_name (locally)
 '''
 def rename_file(file, new_name):
+    log("Renaming " + file + " to " + new_name)
     partition = file.rpartition("/") #https://docs.python.org/3/library/stdtypes.html#str.rpartition
     new_file = partition[0] + partition[1] + new_name
     os.rename(file, new_file)
     #print("should rename " + file + " to " + new_file) #https://docs.python.org/3/library/os.html#os.rename
 
 #Creates OR rewrites a file if it exists
-def create_file(path, content):
+def create_file(path, content, permission=None):
+    log("Creating " + path)    
     make_path(path)
     f = open(path,'w')
     f.write(content)
     f.close()
+    if permission:
+        modify_file_permissions(path, permission)
 
 def edit_file(path, rules, backup=True):
+    log("Editing " + path)    
     backup_file(path)
     content = replace(read_file(path), rules)
     create_file(path, content)
@@ -80,9 +124,14 @@ def sha256_fingerprint(bytes):
 def ssh_keygen(save_to="etc/ssh/", password=None, user="root", host="raspberrypi"):
     keys = {}
     fingerprints = {"sha256": {}}
-    print("generating RSA, DSA, ECDSA keys...")
-    keys["rsa"] = paramiko.RSAKey.generate(4096)
+    rsa_key_bits = 4096
+    dsa_key_bits = 2048
+    ecdsa_key_bits = 521
+    log("Generating " + str(rsa_key_bits) + " RSA key")
+    keys["rsa"] = paramiko.RSAKey.generate(rsa_key_bits)
+    log("Generating " + str(dsa_key_bits) + " DSA key")
     keys["dsa"] = paramiko.DSSKey.generate(2048)
+    log("Generating " + str(ecdsa_key_bits) + " ECDSA key")
     keys["ecdsa"] = paramiko.ECDSAKey.generate(bits=521)
 
     for key, value in keys.items():
@@ -105,6 +154,3 @@ def ssh_keygen(save_to="etc/ssh/", password=None, user="root", host="raspberrypi
 
 def add_quotation(string):
     return "\"" + string + "\""
-
-def activate_raspberry_ssh():
-    pass
--- setup_my_pi.py
+++ setup_my_pi.py
@@ -9,7 +9,7 @@ raspbian_root = "/home/lucas/Coding/zanella_raspberry_addons/raspberrypi_tree_te
 
 #Creates ssh keys on raspbian and generates SHA256 fingerprints
 fingerprints = ssh_keygen(save_to = raspbian_root + "etc/ssh/")
-print(fingerprints)
+#print(fingerprints)
 
 #Configures the SSH file of raspbian
 sshd_config = replace_in_file("file_models/sshd_config", [["Port [0-9]*", "Port 2323"]])
@@ -17,6 +17,9 @@ ssh_config_sd_location = raspbian_root + "etc/ssh/sshd_config"
 create_file(ssh_config_sd_location, sshd_config)
 modify_file_permissions(ssh_config_sd_location, stat.S_IRUSR | stat.S_IWUSR) #Equivalent to 0600. Read: Permissions: https://docs.python.org/3/library/stat.html#stat.S_IRWXU
 
+#SSH service comes deactivated by default on raspibian 
+#Raspibian currently uses RC services for SSH, but the startup process is systemd. A compatibility trick is used: https://unix.stackexchange.com/questions/233468/how-does-systemd-use-etc-init-d-scripts
+enable_rc_service(raspbian_root, "ssh")
 
 #Configures wifi password
 network_ssid = "My_SSID"
@@ -28,12 +31,16 @@ network_auth_alg = "OPEN"
 
 create_file(
     raspbian_root + "etc/wpa_supplicant/wpa_supplicant.conf",
-    "network = {\n\
+    "country=GB\n\
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n\
+update_config=1\n\
+network = {\n\
     ssid=" + add_quotation(network_ssid) + "\n\
     psk=" + add_quotation(network_password) + "\n\
     proto=" + network_proto + "\n\
     key_mgmt=" + network_key_mgmt + "\n\
     pairwise=" + network_pairwise + "\n\
     auth_alg=" + network_auth_alg + "\n\
-}"
+}",
+    permission = stat.S_IRUSR | stat.S_IWUSR
 )
